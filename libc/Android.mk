460a55f7960894712ae244f7e552069a17036b59
Henrik Smiding <henrik.smiding@stericsson.com>
2011-01-17 16:05:41 +0100

    Add optimized version of memcpy for Cortex A9
    Adds new code to memcpy function, optimized for Cortex A9.
    Adds new ARM-only loop, for operations where source and
    destination are aligned.
    
    Copyright (C) ST-Ericsson SA 2010
    
    Modified neon implementation to fit Cortex A9 cache line size,
    for those running 32 bytes L2 cache line size.
    Also split the implementation in aligned and unaligned access,
    for those that allows unaligned memory access with Neon.
    For totally aligned operations, arm-only code is used.
    
    Change-Id: I95ebf6164cd6486b12a7e3e98e369db21e7e18d2
    Author: Henrik Smiding henrik.smiding@stericsson.com for ST-Ericsson.
    Signed-off-by: Christian Bejram <christian.bejram@stericsson.com>
    
    bionic: Make use of new memcpy, tweak settings
    
    The new memcpy() is slightly faster than the cortex-strings version,
    and fixes the Galaxy Nexus camera issue.
    
    Change-Id: Ie65d14992a399e9bc2709a3d710d52b5b1a62e87
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libc/Android.mk
+++ libc/Android.mk
@@ -377,6 +377,7 @@ libc_common_src_files += \
 	arch-arm/bionic/tgkill.S \
 	arch-arm/bionic/memcmp.S \
 	arch-arm/bionic/memcmp16.S \
+	arch-arm/bionic/memcpy.S \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
 	arch-arm/bionic/strcmp.S \
@@ -437,16 +438,13 @@ libc_arch_dynamic_src_files := \
 ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
 libc_common_src_files += \
 	arch-arm/bionic/armv7/memchr.S \
-	arch-arm/bionic/armv7/memcpy.S \
 	arch-arm/bionic/armv7/memset.S \
-	arch-arm/bionic/armv7/bzero.S \
 	arch-arm/bionic/armv7/strchr.S \
 	arch-arm/bionic/armv7/strcpy.c \
 	arch-arm/bionic/armv7/strlen.S
 else
 libc_common_src_files += \
 	string/memchr.c \
-	arch-arm/bionic/memcpy.S \
 	arch-arm/bionic/memset.S \
 	string/strchr.c \
 	arch-arm/bionic/strcpy.S \
@@ -602,6 +600,9 @@ ifeq ($(TARGET_ARCH),arm)
   endif
   ifeq ($(ARCH_ARM_USE_NON_NEON_MEMCPY),true)
     libc_common_cflags += -DARCH_ARM_USE_NON_NEON_MEMCPY
+  endif
+  ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+    libc_common_cflags += -DNEON_UNALIGNED_ACCESS -DNEON_MEMCPY_ALIGNMENT_DIVIDER=224
   endif
 
   # Add in defines to activate SCORPION_NEON_OPTIMIZATION
